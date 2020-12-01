9b7b78d3e0fcbc3141036e92768ff4b87345cd7d
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2020-12-01 20:45:48 +0000

    feat: add more dropdown options

--- components/Dropdown/index.js
+++ components/Dropdown/index.js
@@ -4,7 +4,9 @@ export default function DropDown({ setCategory }) {
   const options = [
     { value: '', label: 'All Categories' },
     { value: 'fintech', label: 'Fintech' },
+    { value: 'payments', label: 'Payments' },
     { value: 'health', label: 'Health' },
+    { value: 'transport', label: 'Transport' },
   ]
   const handleChange = (e) => setCategory(e.target.value)
 
